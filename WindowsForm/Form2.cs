db3b6e0cd8301226215224c66b67b2ec764795aa
José Antonio Balbuena <JoseABalbuena2@hotmail.com>
2022-05-07 21:07:54 -0500

    Cambios Clientes

--- WindowsForm/Form2.cs
+++ WindowsForm/Form2.cs
@@ -23,6 +23,90 @@ namespace WindowsForm
 
         }
 
+        private void SaveUser()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=base_de_datos;";                             // VALUES (NULL, '" + textBox1.Text +
+            string query = "INSERT INTO productos(`id`, `Nombre`, `Apellidos`, `email`, `usuario`,`contraseña`, `telefono`, `direccioncalle`, `poblacion`) VALUES ('" + txtID.Text + "', '" + txtNombre.Text + "', '" + txtApellidos.Text + "', '" + txtemail.Text + "', '" + txtUsuario.Text + "', '" + txtContraseña.Text + "', '" + txtTelefono + "', '" + txtDireccion.Text + "', '" + txtPoblacion.Text + "')";
+            // Que puede ser traducido con un valor a:
+            //INSERT INTO `productos` (`idProducto`, `nombreProducto`, `descripcionProducto`, `precioProducto`, `existenciasProductos`) VALUES ('3456', 'ghfghfgh', 'fghghf', '12', '12');
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Producto insertado satisfactoriamente");
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Mostrar cualquier error
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void UpdateUser()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=user;";
+            // Actualizar la fila user con ID 1
+            string query = "UPDATE  productos`user` SET (`id`, `Nombre`, `Apellidos`, `email`, `usuario`,`contraseña`, `telefono`, `direccioncalle`, `poblacion`) VALUES ('" + txtID.Text + "', '" + txtNombre.Text + "', '" + txtApellidos.Text + "', '" + txtemail.Text + "', '" + txtUsuario.Text + "', '" + txtContraseña.Text + "', '" + txtTelefono + "', '" + txtDireccion.Text + "', '" + txtPoblacion.Text + "')";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                // Actualizado satisfactoriamente
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Ops, quizás el ID no existe
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void DeleteUser()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=user;";
+            // Borrar la fila con ID #?
+
+            int ID = Convert.ToInt32(txtID.Text);
+
+            string query = "DELETE FROM `user` WHERE id = " + ID;
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                // Eliminado satisfactoriamente
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Ops, quizás el id no existe
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         void connect()
         {
             string server = "localhost";
@@ -96,6 +180,36 @@ namespace WindowsForm
         private void Form2_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblemail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonInsertar_Click(object sender, EventArgs e)
+        {
+            SaveUser();
+        }
+
+        private void buttonActualizar_Click(object sender, EventArgs e)
+        {
+            UpdateUser();
+        }
+
+        private void buttonBorrar_Click(object sender, EventArgs e)
+        {
+            DeleteUser();
         }
     }
 }
